8629f6b8be63fdab99c8f3afe6fc4e4231e95a44
James Betker <jbetker@gmail.com>
2022-04-10 19:29:42 -0600

    Add read script

--- do_tts.py
+++ do_tts.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn.functional as F
 import torchaudio
 
-from api_new_autoregressive import TextToSpeech, load_conditioning
+from api import TextToSpeech, load_conditioning
 from utils.audio import load_audio
 from utils.tokenizer import VoiceBpeTokenizer
 
@@ -18,6 +18,7 @@ if __name__ == '__main__':
         'harris': ['voices/harris/1.wav', 'voices/harris/2.wav'],
         'lescault': ['voices/lescault/1.wav', 'voices/lescault/2.wav'],
         'otto': ['voices/otto/1.wav', 'voices/otto/2.wav'],
+        'obama': ['voices/obama/1.wav', 'voices/obama/2.wav'],
         # Female voices
         'atkins': ['voices/atkins/1.wav', 'voices/atkins/2.wav'],
         'grace': ['voices/grace/1.wav', 'voices/grace/2.wav'],
@@ -27,8 +28,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-text', type=str, help='Text to speak.', default="I am a language model that has learned to speak.")
-    parser.add_argument('-voice', type=str, help='Use a preset conditioning voice (defined above). Overrides cond_path.', default='dotrice,harris,lescault,otto,atkins,grace,kennard,mol')
-    parser.add_argument('-num_samples', type=int, help='How many total outputs the autoregressive transformer should produce.', default=32)
+    parser.add_argument('-voice', type=str, help='Use a preset conditioning voice (defined above). Overrides cond_path.', default='obama,dotrice,harris,lescault,otto,atkins,grace,kennard,mol')
+    parser.add_argument('-num_samples', type=int, help='How many total outputs the autoregressive transformer should produce.', default=128)
     parser.add_argument('-batch_size', type=int, help='How many samples to process at once in the autoregressive model.', default=16)
     parser.add_argument('-num_diffusion_samples', type=int, help='Number of outputs that progress to the diffusion stage.', default=16)
     parser.add_argument('-output_path', type=str, help='Where to store outputs.', default='results/')
@@ -38,9 +39,6 @@ if __name__ == '__main__':
     tts = TextToSpeech(autoregressive_batch_size=args.batch_size)
 
     for voice in args.voice.split(','):
-        tokenizer = VoiceBpeTokenizer()
-        text = torch.IntTensor(tokenizer.encode(args.text)).unsqueeze(0).cuda()
-        text = F.pad(text, (0,1))  # This may not be necessary.
         cond_paths = preselected_cond_voices[voice]
         conds = []
         for cond_path in cond_paths:
