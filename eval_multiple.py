64446baebd532294df53ea58e3a72f5878dab4e1
James Betker <jbetker@gmail.com>
2022-04-12 16:40:42 -0600

    Updates

--- eval_multiple.py
+++ eval_multiple.py
@@ -7,7 +7,7 @@ from utils.audio import load_audio
 
 if __name__ == '__main__':
     fname = 'Y:\\libritts\\test-clean\\transcribed-brief-w2v.tsv'
-    outpath = 'D:\\tmp\\tortoise-tts-eval\\eval_new_autoregressive'
+    outpath = 'D:\\tmp\\tortoise-tts-eval\\diverse_auto_256_samp_100_di_4'
     outpath_real = 'D:\\tmp\\tortoise-tts-eval\\real'
 
     os.makedirs(outpath, exist_ok=True)
@@ -24,9 +24,9 @@ if __name__ == '__main__':
         path = os.path.join(os.path.dirname(fname), line[1])
         cond_audio = load_audio(path, 22050)
         torchaudio.save(os.path.join(outpath_real, os.path.basename(line[1])), cond_audio, 22050)
-        sample = tts.tts(transcript, [cond_audio, cond_audio], num_autoregressive_samples=512, k=1,
+        sample = tts.tts(transcript, [cond_audio, cond_audio], num_autoregressive_samples=256, k=1,
                              repetition_penalty=2.0, length_penalty=2, temperature=.5, top_p=.5,
-                             diffusion_temperature=.7, cond_free_k=2, diffusion_iterations=200)
+                             diffusion_temperature=.7, cond_free_k=2, diffusion_iterations=100)
 
         down = torchaudio.functional.resample(sample, 24000, 22050)
         fout_path = os.path.join(outpath, os.path.basename(line[1]))
