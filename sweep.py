3c62becf1e707ef0ff79fbe047460501626131f4
James Betker <jbetker@gmail.com>
2022-04-13 17:03:36 -0600

    Update sweep & eval_multiple with new voices

--- sweep.py
+++ sweep.py
@@ -24,19 +24,24 @@ def permutations(args):
 
 
 if __name__ == '__main__':
-    fname = 'Y:\\libritts\\test-clean\\transcribed-brief-w2v.tsv'
-    outpath_base = 'D:\\tmp\\tortoise-tts-eval\\std_sweep3'
+    fname = 'Y:\\clips\\books2\\subset512-oco.tsv'
+    stop_after = 128
+    outpath_base = 'D:\\tmp\\tortoise-tts-eval\\sweep'
     outpath_real = 'D:\\tmp\\tortoise-tts-eval\\real'
 
     arg_ranges = {
-        'top_p': [.3,.4,.5,.6],
-        'temperature': [.5, .6],
+        'top_p': [.5, 1],
+        'temperature': [.5, 1],
+        'diffusion_temperature': [.6, 1],
+        'cond_free_k': [0, 1, 4],
+        'repetition_penalty': [1.0, 2.0]
     }
     cfgs = permutations(arg_ranges)
     shuffle(cfgs)
 
     for cfg in cfgs:
-        outpath = os.path.join(outpath_base, f'{cfg["top_p"]}_{cfg["temperature"]}')
+        cfg_desc = '_'.join([f'{k}-{v}' for k,v in cfg.items()])
+        outpath = os.path.join(outpath_base, f'{cfg_desc}')
         os.makedirs(outpath, exist_ok=True)
         os.makedirs(outpath_real, exist_ok=True)
         with open(fname, 'r', encoding='utf-8') as f:
@@ -45,15 +50,14 @@ if __name__ == '__main__':
         recorder = open(os.path.join(outpath, 'transcript.tsv'), 'w', encoding='utf-8')
         tts = TextToSpeech()
         for e, line in enumerate(lines):
+            if e >= stop_after:
+                break
             transcript = line[0]
-            if len(transcript) > 120:
-                continue  # We need to support this, but cannot yet.
             path = os.path.join(os.path.dirname(fname), line[1])
             cond_audio = load_audio(path, 22050)
             torchaudio.save(os.path.join(outpath_real, os.path.basename(line[1])), cond_audio, 22050)
-            sample = tts.tts(transcript, [cond_audio, cond_audio], num_autoregressive_samples=256, k=1, diffusion_iterations=200,
-                             repetition_penalty=2.0, length_penalty=2, temperature=.5, top_p=.5,
-                             diffusion_temperature=.7, cond_free_k=2, **cfg)
+            sample = tts.tts(transcript, [cond_audio, cond_audio], num_autoregressive_samples=256,
+                             k=1, diffusion_iterations=70, length_penalty=1.0, **cfg)
             down = torchaudio.functional.resample(sample, 24000, 22050)
             fout_path = os.path.join(outpath, os.path.basename(line[1]))
             torchaudio.save(fout_path, down.squeeze(0), 22050)
