d5bf74f4002388b17bb1107c8c70bf150e6725df
Johan Nordberg <its@johan-nordberg.com>
2022-05-31 15:07:17 +0000

    Include data files in package Fixes #81

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
+import os
+import glob
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+data_dir = 'tortoise/data'
+data_files = [(data_dir, list(glob.glob(os.path.join(data_dir, '*'))))]
+
 setuptools.setup(
     name="TorToiSe",
     packages=setuptools.find_packages(),
+    data_files=data_files,
     version="2.4.0",
     author="James Betker",
     author_email="james@adamant.ai",
