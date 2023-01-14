f309586b5f0b4ffd57b1298eaad53f9672a60790
Dawid <git@dawid.lasz.uk>
2023-01-14 09:49:19 -0800

    TextToSpeech to utilize passed device arg

--- tortoise/api.py
+++ tortoise/api.py
@@ -213,7 +213,9 @@ class TextToSpeech:
         self.models_dir = models_dir
         self.autoregressive_batch_size = pick_best_batch_size_for_gpu() if autoregressive_batch_size is None else autoregressive_batch_size
         self.enable_redaction = enable_redaction
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        if device is None:
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = device
         if self.enable_redaction:
             self.aligner = Wav2VecAlignment()
 
