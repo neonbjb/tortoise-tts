ae75dab49112830ddaa99b08441d34c0d01b9f6b
Super Chiba <25316415+Chapoly1305@users.noreply.github.com>
2023-11-23 13:42:18 +0800

    bug fix, adding args options, adding batch_size option.

--- tortoise/do_tts.py
+++ tortoise/do_tts.py
@@ -12,23 +12,31 @@ if __name__ == '__main__':
     parser.add_argument('--text', type=str, help='Text to speak.', default="The expressiveness of autoregressive transformers is literally nuts! I absolutely adore them.")
     parser.add_argument('--voice', type=str, help='Selects the voice to use for generation. See options in voices/ directory (and add your own!) '
                                                  'Use the & character to join two voices together. Use a comma to perform inference on multiple voices.', default='random')
-    parser.add_argument('--preset', type=str, help='Which voice preset to use.', default='fast')
-    parser.add_argument('--use_deepspeed', type=str, help='Which voice preset to use.', default=False)
-    parser.add_argument('--kv_cache', type=bool, help='If you disable this please wait for a long a time to get the output', default=True)
-    parser.add_argument('--half', type=bool, help="float16(half) precision inference if True it's faster and take less vram and ram", default=True)
+    parser.add_argument('--preset', type=str, help='Which voice preset to use.', default='fast', choices=['high_quality' , 'standard', 'fast', 'ultra_fast'])
+    parser.add_argument('--use_deepspeed', type=bool, help='Which voice preset to use. Default to False', default=False, choices=[True, False])
+    parser.add_argument('--kv_cache', type=bool, help='If you disable this please wait for a long a time to get the output. Default to True', default=True, choices=[True, False])
+    parser.add_argument('--half', type=bool, help="float16(half) precision inference if True it's faster and take less vram and ram. Default to True", default=True, choices=[True, False])
     parser.add_argument('--output_path', type=str, help='Where to store outputs.', default='results/')
     parser.add_argument('--model_dir', type=str, help='Where to find pretrained model checkpoints. Tortoise automatically downloads these to .models, so this'
                                                       'should only be specified if you have custom checkpoints.', default=MODELS_DIR)
-    parser.add_argument('--candidates', type=int, help='How many output candidates to produce per-voice.', default=3)
+    parser.add_argument('--candidates', type=int, help='How many output candidates to produce per-voice. Default to 3', default=3)
     parser.add_argument('--seed', type=int, help='Random seed which can be used to reproduce results.', default=None)
-    parser.add_argument('--produce_debug_state', type=bool, help='Whether or not to produce debug_state.pth, which can aid in reproducing problems. Defaults to true.', default=True)
+    parser.add_argument('--produce_debug_state', type=bool, help='Whether or not to produce debug_state.pth, which can aid in reproducing problems. Defaults to true.', default=True, choices=[True, False])
     parser.add_argument('--cvvp_amount', type=float, help='How much the CVVP model should influence the output.'
                                                           'Increasing this can in some cases reduce the likelihood of multiple speakers. Defaults to 0 (disabled)', default=.0)
+    parser.add_argument('--batch_size', type=int, help='(Optional) If you want to specify the batch size to use for autoregression. Usually, VRAM-2GB if half=True, VRAM/2 if half=False')
     args = parser.parse_args()
-    if torch.backends.mps.is_available():
+    if torch.cuda.is_available():
+        args.use_deepspeed = True
+    else:
         args.use_deepspeed = False
     os.makedirs(args.output_path, exist_ok=True)
-    tts = TextToSpeech(models_dir=args.model_dir, use_deepspeed=args.use_deepspeed, kv_cache=args.kv_cache, half=args.half)
+    if args.batch_size is not None:
+        tts = TextToSpeech(models_dir=args.model_dir, use_deepspeed=args.use_deepspeed, kv_cache=args.kv_cache,
+                           half=args.half, autoregressive_batch_size=args.batch_size)
+    else:
+        tts = TextToSpeech(models_dir=args.model_dir, use_deepspeed=args.use_deepspeed, kv_cache=args.kv_cache,
+                           half=args.half)
 
     selected_voices = args.voice.split(',')
     for k, selected_voice in enumerate(selected_voices):
@@ -37,7 +45,6 @@ if __name__ == '__main__':
         else:
             voice_sel = [selected_voice]
         voice_samples, conditioning_latents = load_voices(voice_sel)
-
         gen, dbg_state = tts.tts_with_preset(args.text, k=args.candidates, voice_samples=voice_samples, conditioning_latents=conditioning_latents,
                                   preset=args.preset, use_deterministic_seed=args.seed, return_deterministic_state=True, cvvp_amount=args.cvvp_amount)
         if isinstance(gen, list):
